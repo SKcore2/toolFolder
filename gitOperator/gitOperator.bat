802a46fa611a59d868b6692d0817d65514970598
Seto Kodai <skcore2@yahoo.co.jp>
2018-01-08 01:08:44 +0900

    テスト

--- gitOperator/gitOperator.bat
+++ gitOperator/gitOperator.bat
@@ -5,10 +5,11 @@ echo %1
 
 set a=3
 if %1==Status (
-echo its 1
 git status > result.txt
 ) else if %1==Add (
 git add %2 > result.txt
 ) else if %1==Commit (
-echo its 3
+git commit -m %2 > result.txt
+) else if %1==Push (
+git push > result.txt
 )
