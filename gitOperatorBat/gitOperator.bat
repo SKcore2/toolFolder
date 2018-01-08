6f42aff7d455cb4e444c4ee79a4512cd4d1c6ac4
Seto Kodai <skcore2@yahoo.co.jp>
2018-01-08 23:24:48 +0900

    テストメッセージ

---
+++ gitOperatorBat/gitOperator.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd %2
+echo %1
+echo %2
+
+set a=3
+if %1==Status (
+git status > result.txt
+) else if %1==Add (
+git add %3 > result.txt
+) else if %1==Commit (
+git commit -m %3 > result.txt
+) else if %1==Push (
+echo git push origin %3 > result.txt
+)
